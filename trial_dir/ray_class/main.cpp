942a769b5a93a29d54e1b6cb12fb43fff3012713
navyanshmahla <navyanshmahla17@gmail.com>
2023-06-18 19:09:36 +0530

    fixed bugs for shades

--- trial_dir/ray_class/main.cpp
+++ trial_dir/ray_class/main.cpp
@@ -2,13 +2,34 @@
 #include "../../HEADERS/SDLTemplate/sdltemplate.h"
 #include "ray.h"
 
-bool hit_sphere(const vec3 center, float radius, const ray& r) {
+/*bool hit_sphere(const vec3 center, float radius, const ray &r)
+{
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+    if (discriminant < 0)
+    {
+        return -1.0;
+    }
+    else
+    {
+        return (-b - sqrt(discriminant)) / (2.0 * a);
+    }
+}*/
+double hit_sphere(const vec3 center, float radius, const ray& r) {
     vec3 oc = r.origin() - center;
     auto a = dot(r.direction(), r.direction());
     auto b = 2.0 * dot(oc, r.direction());
     auto c = dot(oc, oc) - radius*radius;
     auto discriminant = b*b - 4*a*c;
-    return (discriminant > 0);
+
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (-b - sqrt(discriminant) ) / (2.0*a);
+    }
 }
 
 vec3 sphere;
@@ -16,15 +37,21 @@ float sphere_radius;
 
 vec3 color(const ray &r)
 {
-    if(hit_sphere(sphere, sphere_radius, r)) return vec3(1,0,0);
+    auto t = hit_sphere(sphere, sphere_radius, r);
+    if (t > 0.0)
+    {
+        vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0, 0, -1));
+        return 0.5 * vec3(N.x() + 1, N.y() + 1, N.z() + 1);
+    }
     vec3 unit_dir = unit_vector(r.direction());
-    float t = 0.5 * (unit_dir.y() + 1.0);
+
+    t = 0.5 * (unit_dir.y() + 1.0);
     return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
 
 int main()
 {
-    sphere=vec3(0,0,-1);
+    sphere = vec3(0, 0, -1);
     sphere_radius = 0.5;
     int width = 800;
     int height = 400;
